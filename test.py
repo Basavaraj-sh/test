fecfd7feae8d804f96228fe836a6f989a11b70f8
Basavaraj-sh <basavaraj.sh@gmail.com>
2017-07-18 16:48:48 +0530

    2nd commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''
 Case swaping
 '''
-input_string = "hELLO wORLD"
+input_string = "HELLO WORLD"
 string = ''.join([i.lower() if i.isupper() else i.upper() for i in input_string ])
 
 print string
