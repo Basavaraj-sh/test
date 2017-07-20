d4335960e73a0b0633d9e185b48df21291bdcb3e
braj <braj@example.com>
2017-07-20 08:20:32 +0000

    build 1

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 '''
 Case swaping
 '''
-input_string = "This is Second Commit"
-string = ''.join([i.lower() if i.isupper() else i.upper() for i in input_string ])
 
-print string
+print "Build number 1" 
